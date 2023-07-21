1526f0b88340aacd4fd60050a0f8c2487afe0965
carrier1269 <carrier1269@naver.com>
2023-07-21 20:34:18 +0900

    API 설정 완료

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import threading
 import cv2
 from drone_opencv import window
@@ -16,6 +16,10 @@ from my_lib import image_generator_lib
 import os
 from dotenv import load_dotenv
 
+# 캡쳐할 윈도우 창 이름 설정하기
+
+window_capture = "Mirroid"
+
 load_dotenv()
 
 kakao_api_key =  os.environ.get('kakao_api_key')
@@ -25,22 +29,24 @@ app = Flask(__name__)
 output_frame = None
 lock = threading.Lock()
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
 def capture_frames():
     global output_frame
     ESC_KEY = 27
     FRAME_RATE = 120
     SLEEP_TIME = 1 / FRAME_RATE
 
-    capture = window.WindowCapture("drone_python", FRAME_RATE)
+    capture = window.WindowCapture(window_capture, FRAME_RATE)
 
     while True:
         _, frame = capture.screenshot()
         with lock:
             output_frame = frame.copy()
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 def generate_frames():
     global output_frame
@@ -56,16 +62,31 @@ def generate_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encoded_image) + b'\r\n')
 
+@app.route('/')
+def index():
+    # return render_template('index.html')
+    return print('hello!!')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 @app.route('/AI_gen_image', methods=['POST'])
 def generator_image():
     # Google의 STT 기능을 구현한 함수입니다.
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        # print(" 😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀")
+        # print("")
         print("건물에 그리고싶은 원하는 그림을 말해줘! : ")
+        # print("")
+        # print(" 😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀")
+
         audio = r.listen(source)
         
     mySpeech = r.recognize_google(audio, language='ko', show_all=True)
@@ -78,23 +99,25 @@ def generator_image():
         try :
             prompt = mySpeech['alternative'][0]['transcript']
 
-            print(prompt)
+            print(f'음성 인식 : {prompt}')
 
-            prompt = '만화 그림체로 ' + prompt
+            # prompt_purpose = '실제 사진처럼 비슷하게 ' + prompt
 
-            print(prompt)
+            prompt_purpose = prompt
 
-            translated_prompt = translate_lib.en_to_kr(prompt)
+            # print(prompt)
 
-            print(translated_prompt)
+            translated_prompt = translate_lib.en_to_kr(prompt_purpose)
+
+            print(f'음성 인식 번역 : {translated_prompt}')
                     
             AI_Image = image_generator_lib.image_generator_karlo(translated_prompt, kakao_api_key)
 
-            AI_Image.save("generated_AI_Image/generated_img.png")
+            AI_Image.save("yolov5/segment/generated_AI_Image/generated_img.png")
 
             status = True
 
-            return f'{prompt} 그림이 생성되었습니다.'
+            return f'여러분이 주신 명령 : {prompt}\n 그림이 생성되었습니다.'
                 
         except sr.UnknownValueError:
             print("Google 음성 인식이 오디오를 이해할 수 없습니다.")
--- drone_cam.py
+++ drone_cam.py
@@ -7,7 +7,7 @@ ESC_KEY=27
 FRAME_RATE = 120
 SLEEP_TIME = 1/FRAME_RATE
 
-capture = window.WindowCapture("segment",FRAME_RATE)
+capture = window.WindowCapture("Result View",FRAME_RATE)
 
 while True:
     start=time.time()
